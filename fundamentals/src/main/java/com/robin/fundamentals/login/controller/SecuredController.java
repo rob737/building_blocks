dc97d9db8adb98f4f15bf39af0d97bd9877d9ee4
rob737 <robinsrivastava737@gmail.com>
2023-10-12 07:10:38 +0530

    rob737 : Login flow

---
+++ fundamentals/src/main/java/com/robin/fundamentals/login/controller/SecuredController.java
@@ -0,0 +1,15 @@
+package com.robin.fundamentals.login.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecuredController {
+
+    @GetMapping(path = "/secured")
+    public ResponseEntity<String> getSecuredResponse(){
+        return new ResponseEntity<String>("Secured Response",HttpStatus.OK);
+    }
+}
