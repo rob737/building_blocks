91c2cfc9d3638089e3bd72b795654d18a48b5e62
rob737 <robinsrivastava737@gmail.com>
2024-01-18 08:14:33 +0530

    rob737 : Login flow

--- fundamentals/src/main/java/com/robin/fundamentals/login/config/LoginConfiguration.java
+++ fundamentals/src/main/java/com/robin/fundamentals/login/config/LoginConfiguration.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,6 +21,29 @@ public class LoginConfiguration {
                 ).formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+
+        UserDetails admin = User
+                                .withUsername("admin")
+                                .password("1234")
+                                .authorities("admin")
+                                .build();
+
+        UserDetails readUser = User
+                                .withUsername("read_user")
+                                .password("5678")
+                                .authorities("read")
+                                .build();
+
+        return new InMemoryUserDetailsManager(admin,readUser);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 
 }
